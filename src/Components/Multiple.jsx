898df740ff049f16b1550229dfd2ee56167e50c3
rebeizantoine <rebeizantoine@gmail.com>
2024-11-24 15:29:56 +0200

    sad hhad to change this website

--- src/Components/Multiple.jsx
+++ src/Components/Multiple.jsx
@@ -7,12 +7,12 @@ import "../Styles/multiple.css";
 const Multiple = () => {
   return (
     <div className="background-container">
-      <div className="component-shadow">
+      {/* <div className="component-shadow">
         <Section21 />
-      </div>
-      <div className="component-shadow">
+      </div> */}
+      {/* <div className="component-shadow">
         <Gallery />
-      </div>
+      </div> */}
       <div className="component-shadow">
         <EventsandPrograms />
       </div>
